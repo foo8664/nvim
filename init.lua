3e055ba9d2c1d68b66242bffd643d994e2f6bfd5
Diego Evaristo <di.diegoevaristo@gmail.com>
2026-01-19 13:58:26 -0300

    Adding keymap to see directory view

--- init.lua
+++ init.lua
@@ -1,15 +1,23 @@
 vim.g.mapleader = ","
 
+-- the <nop>'s are for training
 vim.keymap.set("i", "jk", "<esc>")
 vim.keymap.set("i", "<esc>", "<nop>")
 vim.keymap.set("i", "<up>", "<nop>")
 vim.keymap.set("i", "<down>", "<nop>")
 vim.keymap.set("i", "<right>", "<nop>")
 vim.keymap.set("i", "<left>", "<nop>")
+
+-- Source and edit config files quickly
 vim.keymap.set("n", "<leader>ev", ":split $MYVIMRC<cr>")
 vim.keymap.set("n", "<leader>sv", ":source $MYVIMRC<cr>")
+-- Removing whitespace at the end of lines
 -- Using [[]] string because it doesn't escape \s
 vim.keymap.set("n", "<leader>ts", [[:%s/\s\+$/<cr>]], { silent = true })
+-- Going back into "directory view" quickly
+vim.g.netrw_bufsettings = "wrap number relativenumber"
+vim.keymap.set("n", "<leader>d", vim.cmd.Ex)
+
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -27,4 +35,5 @@ vim.api.nvim_create_autocmd({"FileType"}, {
 })
 
 vim.opt.syntax = "enable"
-vim.cmd.colorscheme("CC")
+-- vim.cmd.colorscheme("CC")
+vim.cmd.colorscheme("desert")
