7a46f0be18fd7c6a0283cae39411320be59306de
Diego Evaristo <di.diegoevaristo@gmail.com>
2026-01-21 11:15:07 -0300

    Start treesitter on supported langs only

--- init.lua
+++ init.lua
@@ -17,26 +17,31 @@ vim.keymap.set("n", "<leader>ts", [[:%s/\s\+$/<cr>]], { silent = true })
 -- Going back into "directory view" quickly
 vim.g.netrw_bufsettings = "wrap number relativenumber"
 vim.keymap.set("n", "<leader>d", vim.cmd.Ex)
-
+-- Editing my theme
+vim.keymap.set("n", "<leader>th", ":split ~/.config/nvim/colors/CC.lua<cr>")
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.wrap = true
+vim.opt.hlsearch = true
 vim.opt.swapfile = false
 vim.opt.colorcolumn = "80"
 
 -- Tree-sitter config
-treesit = require("nvim-treesitter")
-treesit.install({"c", "python", "bash"})
+local treesit = require("nvim-treesitter")
+treesit.install("all")
 treesit.setup({
 	install_dir = vim.fn.stdpath("data") .. "/site"
 })
 vim.api.nvim_create_autocmd({"FileType"}, {
 	pattern = {"*"},
 	callback = function()
-		vim.treesitter.start()
+		if vim.treesitter.language.add(vim.bo.ft) then
+			vim.treesitter.start()
+		end
 	end,
 })
+
 
 -- To write README's properly
 vim.api.nvim_create_autocmd({"FileType"}, {
